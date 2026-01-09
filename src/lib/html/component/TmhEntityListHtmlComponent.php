c39a3029d75f15b2c2f84d03a67538bd74465977
Tiền mỹ hiệu <tienmyhieu@gmail.com>
2026-01-09 17:22:14 -0500

    page1

--- src/lib/html/component/TmhEntityListHtmlComponent.php
+++ src/lib/html/component/TmhEntityListHtmlComponent.php
@@ -15,6 +15,37 @@ readonly class TmhEntityListHtmlComponent implements TmhHtmlComponent
         $componentNodes = [];
         $componentNodes[] = $this->elementFactory->listTitle($entity['translation']);
         $componentNodes[] = $this->elementFactory->br();
+
+        foreach ($entity['items'] as $listItem) {
+            $componentNodes[] = $this->elementFactory->listItem([$this->transformListItem($listItem)]);
+        }
+        $componentNodes[] = $this->elementFactory->br();
+
         return [$this->elementFactory->entityList($componentNodes)];
+    }
+
+    private function transformListItem(array $listItem): array
+    {
+        return match($listItem['type']) {
+            'route' => $this->routeListItem($listItem['route']),
+            default => $this->textListItem($listItem)
+        };
+    }
+
+    private function routeListItem(array $listItem): array
+    {
+        return $this->elementFactory->listItemLink(
+            [
+                'href' => $listItem['href'],
+                'title' => $listItem['title']
+            ],
+            $listItem['innerHtml']
+        );
+    }
+
+    private function textListItem(array $listItem): array
+    {
+        $class = 'tmh_list_item';
+        return $this->elementFactory->span(['class' => $class], $listItem['translation']);
     }
 }
