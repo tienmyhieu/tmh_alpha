6e7e261e3220e424ae8fc5923b9e0ac532213fac
Tiền mỹ hiệu <tienmyhieu@gmail.com>
2026-01-18 10:40:35 -0500

    nmma

--- src/lib/html/component/TmhEntityListHtmlComponent.php
+++ src/lib/html/component/TmhEntityListHtmlComponent.php
@@ -72,7 +72,7 @@ readonly class TmhEntityListHtmlComponent implements TmhHtmlComponent
 
     private function textListItem(array $listItem, string $language): array
     {
-        $attributes = ['class' => 'tmh_list_item'];
+        $attributes = [];
         $useLanguage = $this->useLanguage($listItem, $language);
         if ($useLanguage) {
             $attributes['lang'] = $listItem['lang'];
